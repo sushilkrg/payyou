dc0ee6a3e9a38bb67d3bc28510db1b67ee692598
Sushil Kumar <sushilkr820@gmail.com>
2025-06-17 15:00:14 +0530

    Changed auth token storage method from auth header to cookies

---
+++ backend/utils/generateTokenAndSetCookie.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+
+export const generateTokenAndSetCookie = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "10d",
+  });
+
+  res.cookie("token", token, {
+    maxAge: 10 * 24 * 60 * 60 * 1000, // 10 days
+    httpOnly: true,
+    // secure: true,
+    // sameSite: "none",
+  });
+};
